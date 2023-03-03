ac18b11d892a2d0eeee21f8e3bc0ce921ced3011
Burak Senturk <senturkburk@gmail.com>
2023-03-03 22:25:13 +0300

    Blog ve Comment de düzenlemeler yapıldı

---
+++ BlogProject/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesslayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjectUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+        public IActionResult BlogReadAll(int id)
+        {
+            var values = bm.GetBlogByID(id);
+            return View(values);
+        }
+    }
+}
---
+++ BlogProject/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesslayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjectUI.Controllers
+{
+	public class CommentController : Controller
+	{
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult PartialAddComment()
+		{
+			return PartialView();
+		}
+        public PartialViewResult CommentListByBlog(int id)  //blogdaki yorum listesi
+        {
+			var values = cm.Getlist(id);
+            return PartialView(values);
+        }
+    }
+}
